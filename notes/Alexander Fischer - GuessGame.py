9af72e1470dbd06beb8d13e2803e5fd4e0b423dd
blondfisch <42385460+blondfisch@users.noreply.github.com>
2018-11-02 09:45:52 -0700

    11.2.18v2

--- notes/Alexander Fischer - GuessGame.py
+++ notes/Alexander Fischer - GuessGame.py
@@ -3,30 +3,28 @@ difficulty = input("Type in difficulty; Easy or Hard")
 print("%s" % difficulty)
 
 if difficulty.lower() == "easy":
-    print("I have a random number between 0 and 10. You have 10 guesses to figure it out.")
+    print("I have a random number between 0 and 10. You have 5 guesses to figure it out.")
     number = random.randint(0, 10)
 else:
     number = random.randint(0, 100)
-    print("I have a random number between 0 and 100. You have 10 guesses to figure it out.")
+    print("I have a random number between 0 and 100. You have 5 guesses to figure it out.")
 
 guess = int(input("Type in a number"))
 amount_guess = 0
+playing = True
 
-while amount_guess < 10:
+while amount_guess < 5 and playing:
     if number > guess:
         print("Guess higher")
         guess = int(input("Guess again"))
         amount_guess = amount_guess + 1
     elif number == guess:
-        amount_guess = 11
+        print("Congratulations! You won!")
+        playing = False
     else:
         print("Guess lower.")
         guess = int(input("Guess again"))
         amount_guess = amount_guess + 1
 
-if amount_guess == 10:
+if amount_guess == 5:
     print("You ran out of guesses. Heisenwiebe won.")
-
-if amount_guess == 11:
-    print("Congratulations! You won!")
-
