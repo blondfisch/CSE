ac5b867cccf42e8e1f88aeef24c3be80a9090030
blondfisch <42385460+blondfisch@users.noreply.github.com>
2018-11-08 09:46:14 -0800

    11.8.18

--- notes/Alexander Fischer - Hangman.py
+++ notes/Alexander Fischer - Hangman.py
@@ -1,3 +1,14 @@
 import random
-myList = ("Demographic", "extinguish", "square", "pizza", "animal",)
-word = random.randint(0,5)
+wordList = [["e", "d", "i", "s", "o", "n"], ["t", "i", "g", "e", "r"], ["w", "i", "e", "b", "e"]]
+wordidx = random.randint(0, 2)
+word = wordList[wordidx]
+print(word)
+guesseslft = 6
+while guesseslft > 0:
+    print("Guesses left: %d" % guesseslft)
+    guessltr = input("Guess a letter")
+    if guessltr in [word]:
+        print(correct)
+    else:
+        print("Incorrect")
+        guesseslft -= 1
--- notes/Alexander Fischer - Lucky 7s.py
+++ notes/Alexander Fischer - Lucky 7s.py
@@ -1,25 +1,25 @@
 import random
 cwallet = 15
 round_count = 0
-mwallet = 0
+mwallet = 15
+best_round = 0
 print("Welcome to Lucky 7's, the gambling game in which Heisenwiebe rips all of you money from your hands!")
 print("Time to make a bet! You have made a $1 bet and if your role equals 7, you win it back, plus $4 extra dollars!")
 while cwallet > 0:
-    if mwallet <= cwallet: # THis is the problem, you just need to figure out where this code goes
-        mwallet = cwallet
-        bet = input("Type in bet to play")
-        if bet.lower() == "bet":
             dice_1 = random.randint(1, 6)
             dice_2 = random.randint(1, 6)
             roll = dice_1 + dice_2
             if roll == 7:
                 print("Congratulations! You won this round")
-                print("The dice rolled a/an %d" % roll)
+                print("The dice rolled a %d" % roll)
                 cwallet += 4
                 dice_1 = random.randint(1, 6)
                 dice_2 = random.randint(1, 6)
                 round_count += 1
                 print("You currently have $%d" % cwallet)
+                if mwallet < cwallet:
+                    mwallet = cwallet
+                    best_round = round_count
             else:
                 print("The dice rolled a/an %d" % roll)
                 print("So sad. Heisenwiebe took your money this round.")
@@ -28,3 +28,7 @@ while cwallet > 0:
                 dice_1 = random.randint(1, 6)
                 dice_2 = random.randint(1, 6)
                 round_count += 1
+
+print("You had a maximum amount of $%d" % mwallet)
+print("You lasted %d rounds" % round_count)
+print("You should have stopped after round %d" % best_round)
