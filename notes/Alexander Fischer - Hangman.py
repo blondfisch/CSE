956fe388707ded09dc831a125c86aa73e59f737c
blondfisch <42385460+blondfisch@users.noreply.github.com>
2018-11-09 10:36:56 -0800

    11.9.18

--- notes/Alexander Fischer - Hangman.py
+++ notes/Alexander Fischer - Hangman.py
@@ -1,14 +1,20 @@
 import random
 wordList = [['e', 'd', 'i', 's', 'o', 'n'], ['t', 'i', 'g', 'e', 'r'], ['w', 'i', 'e', 'b', 'e']]
-wordidx = random.randint(0, 2)
+wordidx = random.randint(0, len(wordList) - 1)
 word = wordList[wordidx]
-print(word)
+#   print(word)
 guesseslft = 6
-while guesseslft > 0:
+letterslft = 0
+len(wordList[wordidx])  # This is the length of the list
+while guesseslft > 0 and letterslft > 0:
     print("Guesses left: %d" % guesseslft)
+    print("Letters left: %d" % letterslft)
     guessltr = input("Guess a letter")
-    if guessltr in [word]:
+    if guessltr is [word]:
         print("Correct")
     else:
         print("Incorrect")
         guesseslft -= 1
+if guesseslft == 0:
+    print("You ran out of turns. Heisenwiebe won.")
+    print("The actual word was %s" % word)
