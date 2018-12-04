5e28cb500ed395f7e2e53d97af5c09b6a8a60a56
blondfisch <42385460+blondfisch@users.noreply.github.com>
2018-12-04 09:45:02 -0800

    12.4.18

--- notes/Challenges.py
+++ notes/Challenges.py
@@ -1,5 +1,7 @@
 import random
 import string
+import datetime
+import math
 
 
 def challenge1(first_name, last_name):
@@ -11,9 +13,9 @@ print(challenge1("Alex", "Fischer"))
 
 def challenge2(a):
     if a % 2 == 0:
-        print("The number is even.")
+        return "The number is even."
     else:
-        print("The number is odd.")
+        return "The number is odd."
 
 
 print(challenge2(4))
@@ -28,25 +30,25 @@ print(challenge3(5, 6))
 
 def challenge4(a):
     if a >= 1:
-        print("The number is positive")
+        return "The number is positive"
     elif a == 0:
-        print("The number is 0")
+        return "The number is 0"
     else:
-        print("The number is negative.")
+        return "The number is negative."
 
 
 print(challenge4(-5))
 
 
 def challenge5(radius):
-    return 2*radius*3.1415
+    return math.pi * radius ** 3
 
 
 print("The circle has an area of %s units." % challenge5(4))
 
 
 def challenge6(radi):
-    return 4/3 * radi * 3.1415
+    return 4/3 * radi * math.pi
 
 
 print(challenge6(5))
@@ -61,11 +63,11 @@ print(challenge7(1))
 
 def challenge8(number):
     if number in range(1850, 2150):
-        print("The number is within 150 of 2000")
+        return "The number is within 150 of 2000"
     elif number in range(2850, 3150):
-        print("That number is within 150 of 3000")
+        return "That number is within 150 of 3000"
     else:
-        print("That number is not even close to 2000 or 3000")
+        return "That number is not even close to 2000 or 3000"
 
 
 print(challenge8(3001))
@@ -74,12 +76,31 @@ print(challenge8(3001))
 def challenge9(letter):
     vowel_list = ["a", "e", "i", "o", "u"]
     if letter in vowel_list:
-        print("That is a vowel")
+        return "That is a vowel"
     else:
-        print("The letter is not a vowel")
+        return "The letter is not a vowel"
 
 
 print(challenge9("a"))
 
 
-def challenge10():
+def challenge10(integer):
+    try:
+        int(integer)
+        return True
+    except ValueError:
+        return False
+
+
+print(challenge10(31))
+
+
+def challenge11(a, b, c, d, e, f, g):
+    datetime.datetime.now
+
+
+def challenge12(a,b):
+    return math.gcd(a, b)
+
+
+print(challenge12(4, 8))
