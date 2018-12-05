48859d31a151d9c0b858c94afbd446956ca86904
blondfisch <42385460+blondfisch@users.noreply.github.com>
2018-12-05 09:47:42 -0800

    e5

---
+++ notes/Trash File.py
@@ -0,0 +1,19 @@
+import random
+import math
+import string
+import datetime
+
+
+def challenge1():
+
+    numbers = []
+    ",".join[numbers]
+    for i in range(2000, 3200):
+        if i % 7 == 0 and not i % 7 == 0:
+            numbers.append(i)
+            print(numbers)
+        else:
+            return False
+
+
+print(challenge1())
