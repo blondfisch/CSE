144174a07e785b207f772aba88d12f9f2b031199
blondfisch <42385460+blondfisch@users.noreply.github.com>
2018-12-06 09:45:54 -0800

    12.6.18

--- notes/Trash File.py
+++ notes/Trash File.py
@@ -11,4 +11,21 @@ def challenge1():
             numbers.append(i)
             print(numbers)
 
+
 print(challenge1())
+
+
+print(math.factorial(8))
+
+
+def challenge2(a, b, c, d, e, f, g):
+    list_boi = [a, b, c, d, e, f, g]
+    tuple_boi = (a, b, c, d, e, f, g)
+    print(tuple_boi)
+    return list_boi
+
+
+print(challenge2(3, 4, 5, 6, 8, 10, 60))
+
+
+theThing = input("T series will rule them all.")
