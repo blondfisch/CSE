519d460c587a5856cb06a73323d8f0a89d528349
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-01-10 13:35:59 -0800

    halla

--- notes/Alexander Fischer - Hangman.py
+++ notes/Alexander Fischer - Hangman.py
@@ -8,6 +8,9 @@ guesseslft = 8
 prior_guesses = []
 legal_letters = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 print(legal_letters)
+oppo_letters = word.swapcase()
+anti_letters = list(oppo_letters)
+print(oppo_letters)
 disp_list = []
 for i in range(len(word_listform)):
     if word_listform[i] in legal_letters:
@@ -18,9 +21,14 @@ while guesseslft > 0 and "_" in disp_list:
     print(' '.join(disp_list))
     print("Guesses left: %d" % guesseslft)
     guessltr = input("Guess a letter")
-    if guessltr not in prior_guesses and guessltr in legal_letters and guessltr in word_listform or :
+    if guessltr not in prior_guesses and guessltr in legal_letters and guessltr in word_listform:
         for letter in range(len(word_listform)):
             if guessltr == word_listform[letter]:
+                disp_list[letter] = word_listform[letter]
+                prior_guesses.append(guessltr)
+    elif guessltr in anti_letters:
+        for letter in range(len(oppo_letters)):
+            if guessltr == anti_letters[letter]:
                 disp_list[letter] = word_listform[letter]
                 prior_guesses.append(guessltr)
     else:
