bb28d8faf6f19a16bf168d35df207b68de526a10
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-01-14 09:45:22 -0800

    pain

--- notes/Alexander Fischer - Hangman.py
+++ notes/Alexander Fischer - Hangman.py
@@ -11,35 +11,65 @@ print(legal_letters)
 oppo_letters = word.swapcase()
 anti_letters = list(oppo_letters)
 print(oppo_letters)
+totalword = anti_letters + word_listform
+attsolve = False
 disp_list = []
+disp_listcopy = []
 for i in range(len(word_listform)):
     if word_listform[i] in legal_letters:
         disp_list += "_"
     else:
         disp_list += word_listform[i]
+solve = False
+num = 0
+
+'''
+def copy():
+    disp_listcopy = disp_list
+
+
+def restore():
+    disp_list = disp_listcopy
+'''
+
 while guesseslft > 0 and "_" in disp_list:
     print(' '.join(disp_list))
+    attsolve = False
     print("Guesses left: %d" % guesseslft)
-    guessltr = input("Guess a letter")
-    if guessltr not in prior_guesses and guessltr in legal_letters and guessltr in word_listform:
-        for letter in range(len(word_listform)):
-            if guessltr == word_listform[letter]:
-                disp_list[letter] = word_listform[letter]
-                prior_guesses.append(guessltr)
-    elif guessltr in anti_letters:
-        for letter in range(len(oppo_letters)):
-            if guessltr == anti_letters[letter]:
-                disp_list[letter] = word_listform[letter]
-                prior_guesses.append(guessltr)
-    elif guessltr in prior_guesses:
-        print("You already guessed that.")
-    if guessltr not in word_listform:
-        guesseslft -= 1
-        print("Incorrect")
-        prior_guesses.append(guessltr)
-if guesseslft == 0:
-    print("You ran out of turns. Heisenwiebe won.")
-    print("The actual word was %s" % word)
+    asksolve = input("Would you like to solve the entire word? Y for yes N for no:")
+    if asksolve == "Y" or asksolve == "y":
+        attsolve = True
+        guesswrd = input("Enter te word")
+        guesswrd = list(guesswrd)
+        for i in range(len(word_listform)):
+            if guesswrd[i] == word_listform[i] or guesswrd[i] == anti_letters[i]:
+                disp_list[i] = word_listform[i]
+                num += 1
+
+        if num == len(word_listform):
+            solve = True
+        else:
+            print("Incorect")
+            guesseslft -= 1
+             disp_list = disp_listcopy
+    if "_" in disp_list and solve is False and attsolve is False:
+        guessltr = input("Guess a letter")
+        if guessltr not in prior_guesses and guessltr in legal_letters and guessltr in totalword:
+            for letter in range(len(word_listform)):
+                if guessltr == word_listform[letter]:
+                    disp_list[letter] = word_listform[letter]
+                    prior_guesses.append(guessltr)
+
+        elif guessltr not in anti_letters and guessltr in legal_letters and guessltr not in word_listform:
+            print("Incorrect")
+        elif guessltr in prior_guesses:
+            print("You already guessed that.")
+        elif guessltr not in legal_letters:
+            guesseslft -= 1
+            print("That is not a valid letter")
+    if guesseslft == 0:
+        print("You ran out of turns. Heisenwiebe won.")
+        print("The actual word was %s" % word)
 if "_" not in disp_list:
-    print("Congratulations! You guessed the word!")
-    print("The word was %s" % word)
+        print("Congratulations! You guessed the word!")
+        print("The word was %s" % word)
