6242cc2b8edfc5829644c0fa23bcf1a2a3e4c5c1
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-01-16 13:25:00 -0800

    maybe it's done. It nevver can be done.

--- notes/Alexander Fischer - Hangman.py
+++ notes/Alexander Fischer - Hangman.py
@@ -7,7 +7,7 @@ word_listform = list(word)
 guesseslft = 8
 prior_guesses = []
 legal_letters = list(string.ascii_letters)
-anti_letters = list(word.lower())
+anti_letters = list(word.swapcase())
 totalword = anti_letters + word_listform
 disp_list = []
 
@@ -39,10 +39,10 @@ while guesseslft > 0 and "_" in disp_list and solve is False:
     elif "_" in disp_list and len(guessltr) == 1:
         if guessltr not in prior_guesses and guessltr in legal_letters and guessltr in totalword:
             for letter in range(len(word_listform)):
-                if guessltr.lower() == word_listform[letter].lower():
+                if guessltr == word_listform[letter] or guessltr == anti_letters[letter]:
                     disp_list[letter] = word_listform[letter]
                     prior_guesses.append(guessltr)
-
+                    prior_guesses.append(guessltr.swapcase())
         elif guessltr not in anti_letters and guessltr in legal_letters and guessltr not in word_listform:
             print("Incorrect")
             prior_guesses.append(guessltr)
