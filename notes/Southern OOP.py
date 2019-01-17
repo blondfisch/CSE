56c60219be177ef0517fc355098de23ac0fa5f7b
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-01-17 09:44:34 -0800

    the dogs

---
+++ notes/Southern OOP.py
@@ -0,0 +1,23 @@
+class Dog:
+
+    species = 'mammal'
+
+    # Initializer / Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# Instantiate the Dog object
+philo = Dog("Philo", 5)
+mikey = Dog("Mikey", 6)
+Jak = Dog("Jak", 14)
+Tom = Dog("Tom", 4)
+Young_Joe = Dog("Young Joe", 2)
+# Access the instance attributes
+print("{} is {} and {} is {}.".format(
+    philo.name, philo.age, mikey.name, mikey.age))
+
+# Is Philo a mammal?
+if philo.species == "mammal":
+    print("{} is a {}!".format(philo.name, philo.species))
