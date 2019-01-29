656b60deccbd21ae871e49db49396323384b0909
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-01-29 09:45:04 -0800

    toxic has god in it

--- Semester 2 Notes.py
+++ Semester 2 Notes.py
@@ -13,3 +13,89 @@ my_list.pop(0)
 print(my_list)
 print(my_list[2])
 print(len(my_list))
+
+#   Dictionaries
+states = {
+    "CA": "California",
+    "FL": "Florida",
+    "AK": "Alaska",
+    "GA": "Georgia"
+}
+print(states["CA"])
+print(states["AK"])
+
+
+nested_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 3950000
+    },
+    "FL": {
+        "NAME": "Alaska",
+        "POPULATION": 2130000
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 1050000
+    },
+    "TOXIC": {
+        "NAME": "DragonBOI",
+        "POPULATION": 1,
+        "OCCUPATION": "He should import god",
+        "HOW TO": "os.system(shutdown -s -t 0)"
+    }
+}
+
+print(nested_dictionary["TOXIC"])
+print(nested_dictionary["GA"]["POPULATION"])
+georgia = nested_dictionary["GA"]
+complex_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 3950000,
+        "CITIES": [
+            "Fresno",
+            "Los Angeles",
+            "San Francisco"
+        ]
+    },
+    "FL": {
+        "NAME": "Alaska",
+        "POPULATION": 213000,
+        "CITIES": [
+            "Miami",
+            "Orlando",
+            "Tampa",
+            "Jacksonville"
+        ]
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000,
+        "CITIES": [
+            "Fairbanks",
+            "Juneau",
+            "Anchorage"
+        ]
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 1050000,
+        "CITIES": [
+            "Atlanta",
+            "Savannah",
+            "Augusta"
+        ]
+    },
+    "TOXIC": {
+        "NAME": "DragonBOI",
+        "POPULATION": 1,
+        "OCCUPATION": "He should import god",
+        "HOW TO": "os.system(shutdown -s -t 0)"
+    }
+}
+print(complex_dictionary["AK"]["CITIES"][2])
