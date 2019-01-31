c803bd2718bec42edbe8ea0c4cda55badb1395bd
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-01-31 09:45:24 -0800

    stuff

--- Alexander Fischer - Dictionary World Map.py
+++ Alexander Fischer - Dictionary World Map.py
@@ -4,7 +4,7 @@ world_map = {
     'R19A': {
         "NAME": "Mr. Weibe's Room",
         'DESCRIPTION': 'The room of despair, where Heisenwiebe reigns supreme. He is omnipotent and can only be killed'
-                       'by calling God.',
+                       ' by calling God.',
         'PATHS': {
             "NORTH": "SIETCH"
         }
@@ -21,9 +21,23 @@ world_map = {
 # CONTROLLER
 playing = True
 current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+short_directions = ['n', 's', 'e', 'w', 'u', 'd']
 while playing:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input(">_")
-    if command.lower() in ["p", "quit", "exit"]:
+    if command.lower() in short_directions:
+        index = short_directions.index(command.lower())
+        command = directions[index]
+
+    if command.lower() in ["q", "quit", "exit"]:
         playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command Not Found")
