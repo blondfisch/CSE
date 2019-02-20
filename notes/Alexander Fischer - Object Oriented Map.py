4952014ecc25775435e351c8a314f6e150dd1f55
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-02-20 09:45:51 -0800

    jack's rage

--- notes/Alexander Fischer - Object Oriented Map.py
+++ notes/Alexander Fischer - Object Oriented Map.py
@@ -1,4 +1,20 @@
-import random
-
 class Room(object):
-    def __init__(self):
+    def __init__(self, name, descrip, north=None, south=None, east=None, west=None, up=None, down=None):
+        self.name = name
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+        self.up = up
+        self.down = down
+        self.descrip = descrip
+
+
+# Option 2 - set all at once, modify controller
+DESERT1 = Room("Open Desert", 'The sun beats down on the sandy desert all around you. There are caves to the north'
+                              ' inside of the rock, a place of safety from the worms in the desert. On all other sides'
+                              ' is the desert.', "SIETCH", 'DESERT2', 'DESERT3', 'DESERT4')
+SIETCH = Room("Sietch Tabr", 'You walk into a hidden Fremen cave. Inside, you find a Stillsuit folded in a '
+                             'corner, as well as paths leading deeper into the chamber'
+                             ' to the north and south.', None, 'DESERT1', 'SPICE_ROOMS', 'FREMEN_PIT')
+SPICE_ROOMS = Room("Spice Rooms", "Spice is stacked in boxes")
