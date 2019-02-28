3b18eff39c11541bb2be9753da4d35d27c63c7f3
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-02-28 09:44:51 -0800

    cake the distance

--- notes/Special_Random.py
+++ notes/Special_Random.py
@@ -1,7 +1,14 @@
-import random
-
-
-class RandomWiebe:
-    @staticmethod
-    def my_random():
-        return random.randint(1, 100)
+i = 1
+x = 15
+j = 1
+while j == 1:
+    input("Type when Jac's done")
+    print("I disagree about your disagreement")
+    x += 1
+    while i <= x:
+        print("about my disagreement")
+        print("about your disagreement")
+        i += 1
+    x += 1
+    i = 1
+    print("about my disagreement.")
