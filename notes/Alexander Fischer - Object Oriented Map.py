d892824092fcdb513677cfb98552307778d85506
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-03-05 09:44:25 -0800

    dora is a computer program; created more subclasses

--- notes/Alexander Fischer - Object Oriented Map.py
+++ notes/Alexander Fischer - Object Oriented Map.py
@@ -1,4 +1,3 @@
-import
 class Room(object):
     def __init__(self, name, desc, north=None, south=None, east=None, west=None, up=None, down=None, characters=None):
         self.name = name
--- notes/Alexander Fischer - Objects.py
+++ notes/Alexander Fischer - Objects.py
@@ -4,23 +4,46 @@ class Item(object):
 
 
 class Weapon(Item):
-    def __init__(self, damage, range):
-        super(Weapon, self).__init__(damage)
+    def __init__(self, name, damage, distance):
+        super(Weapon, self).__init__(name)
         self.damage = damage
-        self.range = range
+        self.range = distance
 
 
 class Sword(Weapon):
-    def __init__(self, name, damage, shield=True):
-        super(Sword, self).__init__(name, damage, shield)
+    def __init__(self, name, damage, shield):
+        super(Sword, self).__init__("Sword", 10, 0)
         self.name = name
         self.shield = shield
         self.damage = damage
+        self.distance = 0
 
 
-class Lasgun(Weapon):
-    def __init__(self, name, damage, shield=False):
-        super(Lasgun, self).(name, damage, shield)
+class Gun(Weapon):
+    def __init__(self, name, damage, shield):
+        super(Gun, self).__init__("Lasgun", 15, 1)
         self.name = name
-        self.shield = shield
         self.damage = damage
+        self.shield = shield
+        self.distance = 1
+
+
+class Consumable(Item):
+    def __init__(self, name, description):
+        super(Consumable, self).__init__(name)
+        self.name = name
+        self.desc = description
+
+
+class Food(Consumable):
+    def __init__(self, name, health, description):
+        super(Food, self).__init__(name, description)
+        self.name = name
+        self.heal = health
+        self.description = description
+
+
+class Armor(Item):
+    def __init__(self, name, type, defense):
+        super(Armor, self).__init__(name)
+        self.name = name
