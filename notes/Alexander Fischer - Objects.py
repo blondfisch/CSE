c5243a3484ad84cc97e63bb14879b94ee780867d
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-03-08 09:42:59 -0800

    half-life

--- notes/Alexander Fischer - Objects.py
+++ notes/Alexander Fischer - Objects.py
@@ -11,12 +11,13 @@ class Weapon(Item):
 
 
 class Sword(Weapon):
-    def __init__(self, name, damage, durability):
+    def __init__(self, name, damage, durability, desc):
         super(Sword, self).__init__(name, damage, 0)
         self.name = name
         self.damage = damage
         self.distance = 0
         self.durability = durability
+        self.desc = desc
 
     def swing(self):
         if self.durability <= 0:
@@ -27,7 +28,20 @@ class Sword(Weapon):
 
 class Rapier(Sword):
     def __init__(self):
-        super(Rapier, self).__init__("Rapier", 7, 50)
+        super(Rapier, self).__init__("Rapier", 7, 50, "A sharp rapier sword. It is in excellent condition and will"
+                                                      " do massive damage to enemies.")
+
+
+class DullSword(Sword):
+    def __init__(self):
+        super(DullSword, self).__init__("Dull Sword", 2, 25, "A dull sword. It has not been well maintained and is on"
+                                                             " the verge of breaking.")
+
+
+class BroadSword(Sword):
+    def __init__(self):
+        super(BroadSword, self).__init__("Broadsword", 10, 40, "This sword is quite heavy and will wear out quickly"
+                                                               " but does massive damage.")
 
 
 class Gun(Weapon):
@@ -67,6 +81,28 @@ class Water(Consumable):
         super(Water, self).__init__("Water", "Arguably the most valuable item on the planet, water heals you massive"
                                              " amounts but is very rare.")
 
+
+class Rice(Consumable):
+    def __init__(self):
+        super(Rice, self).__init__("Rice", "This item can be eaten for health. It is rather common on the planet.")
+
+
+class Chicken(Consumable):
+    def __init__(self):
+        super(Chicken, self).__init__("Fried Chicken", "The most holy of items, fried chicken. This item heals for all"
+                                                       " of your health.")
+
+
+class Herb(Consumable):
+    def __init__(self):
+        super(Herb, self).__init__("Herbs", "Common Herbs. They are plentiful but heal for only a small amount")
+
+
+class Life(Consumable):
+    def __init__(self):
+        super(Life, self).__init__("Water of Life", "An incredibly dangerous item. Consuming the Water of Life could "
+                                                    "kill you or grant you superhuman strength.")
+        
 
 class Armor(Item):
     def __init__(self, name, desc, defense, shield):
