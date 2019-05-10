d8f080f69b95a19ee0c3b64f295af0690a2747da
blondfisch <42385460+blondfisch@users.noreply.github.com>
2019-05-10 10:41:26 -0700

    yuy

--- Alexander Fischer - Ocean's Github's Endgame2.py
+++ Alexander Fischer - Ocean's Github's Endgame2.py
@@ -2,19 +2,29 @@ import csv
 
 items = {}
 
-
+averages = {}
 with open("Sales Records.csv", "r") as old_csv:
     reader = csv.reader(old_csv)
+    print("Wiebelord......")
     for row in reader:
         if row[0] == 'Region':
             continue
         item_type = row[2]
         profit = float(row[13])
+        units = float(row[8])
         try:
-            items[item_type] += profit
+            items[item_type]["profits"] += profit
+            items[item_type]["units"] += units
         except KeyError:
-            items[item_type] = profit
+            items[item_type] = {}
+            averages[item_type] = {}
+            items[item_type]["profits"] = profit
+            items[item_type]["units"] = units
 
+        for item in items.items():
+            averages[item]["average"] = profit / units
 for key, item in items.items():
     print(key, end=": ")
-    print("${:,}".format(round(item, 2)))
+    print("${:,}".format(round(item["profits"], 2)))
+    print()
+print(items)
